f36029b249ac720f662c7e71fd6a96fe13d83f3e
binllionaire <bug20474@gmail.com>
2023-11-24 21:35:11 +0900

    init : 배포 세팅

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,5 +1,6 @@
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Home } from "../pages/Home.jsx";
+import Home from "../pages/Home"; // Correct the relative path
 
 const Router = () => {
   return (
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 const Home = () => {
   return <p>DO-SOPT-SOPKATHON-WEB-1</p>;
 };
