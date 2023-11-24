c25d83cfaa8a2d5e893961c3cbf3e988dbc792f4
binllionaire <bug20474@gmail.com>
2023-11-24 21:21:47 +0900

    init : 배포 세팅

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Home } from "../pages/Home";
-// import { BrowserRouter, Routes } from "react-router-dom";
+import { Home } from "../pages/Home.jsx";
 
 const Router = () => {
   return (
