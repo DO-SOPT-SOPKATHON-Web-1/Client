586ea4b44f28511672b95832dc8a299650769ee2
binllionaire <bug20474@gmail.com>
2023-11-24 22:31:21 +0900

    chore : 주석 제거

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "../pages/Home"; // Correct the relative path
+import Home from "../pages/Home";
 
 const Router = () => {
   return (
