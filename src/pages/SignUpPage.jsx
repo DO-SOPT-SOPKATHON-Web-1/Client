39cf97a3002d97079a9aad8eeb943e55664d62c2
binllionaire <bug20474@gmail.com>
2023-11-26 08:33:10 +0900

    Revert

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -45,8 +45,8 @@ function SignUpPage() {
           name: username,
         },
       );
-      console.log(response.data.data.userId);
-      navigate(`/main/${response.data.data.userId}`);
+      console.log(response);
+      navigate(`/main/${userId}`);
     } catch (error) {
       console.log(error);
     }
@@ -60,7 +60,7 @@ function SignUpPage() {
           name: username,
         },
       );
-
+      setUserId(createUserResponse.data.data.userId);
       setStep(2);
     } catch (error) {
       if (error.response.status === 409) {
