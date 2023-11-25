006bdec13e9b3655bff5491e49c46b8ed0d75f63
binllionaire <bug20474@gmail.com>
2023-11-26 00:45:28 +0900

    feat : onboarding page UI 구현중

--- src/pages/OnBoardingPage.jsx
+++ src/pages/OnBoardingPage.jsx
@@ -1,5 +1,23 @@
+import styled from "styled-components";
+
+import LongLogo from "../assets/icons/Longlogo.svg";
+
 function OnBoardingPage() {
-  return <div />;
+  return (
+    <Wrapper>
+      <Logo src={LongLogo} alt="HAPPY DEATH DAY" />
+    </Wrapper>
+  );
 }
 
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  background: linear-gradient(175deg, #070707 78.1%, #ff84ad 114.18%);
+`;
+const Logo = styled.img`
+  display: block;
+  margin: 0 auto;
+  width: 200px;
+`;
 export default OnBoardingPage;
