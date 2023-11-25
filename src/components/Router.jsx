6753ef18c0c02831f52c43462396863ab09b4b2d
Soyoung Park <soyoung008@naver.com>
2023-11-26 01:19:00 +0900

    design : 회원가입 ui 제작 중

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import OnBoardingPage from "../pages/OnBoardingPage";
+import SignUpPage from "../pages/SignUpPage";
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<OnBoardingPage />} />
+        <Route path="/" element={<SignUpPage />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,5 +1,28 @@
-function SignUpPage() {
-  return <div />;
-}
+import styled from "styled-components";
 
+function SignUpPage() {
+  return (
+    <SignUpBox>
+      <ProgressBar value="50" min="0" max="100" />
+      <Header>케이크의 주인은 누구인가요? </Header>
+      <InputBox>
+        <Input placeholder="이름을 입력해주세요." />
+        <Input placeholder="이메일을 입력해주세요." />
+      </InputBox>
+      <Button type="button">다음</Button>
+    </SignUpBox>
+  );
+}
+const SignUpBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 3.2rem;
+`;
+const ProgressBar = styled.progress``;
+const Header = styled.div``;
+const InputBox = styled.div`
+  display: flex;
+`;
+const Input = styled.input``;
+const Button = styled.button``;
 export default SignUpPage;
